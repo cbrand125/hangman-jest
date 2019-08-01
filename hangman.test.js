10dadbd3ee0ecaef1d1d2a3a2cd00f3f6581b229
cbrand125 <cbrand@talentpath.com>
2019-08-01 16:06:01 -0700

    add hangman tests

--- hangman.test.js
+++ hangman.test.js
@@ -1,3 +1,35 @@
-describe('play', () => {
-  it('should work', () => {});
+const hangman = require('./hangman');
+
+describe('playHangman', () => {
+  console.log = jest.fn();
+  const hangmanSpy = jest.spyOn(hangman, 'playHangman');
+
+  // resets our mocks after each test
+  afterEach(() => {
+    console.log.mockClear();
+    hangmanSpy.mockClear();
+  });
+
+  it('should print a success message and end when a completed word has been passed', () => {
+    const targetWord = 'abc';
+    const guessedWord = 'abc'.split('');
+    hangman.playHangman(targetWord, guessedWord);
+
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(`You win! The word was ${targetWord}`);
+    expect(hangmanSpy).toBeCalledTimes(1);
+  });
+
+  it('should print a failure message and end when you have run out of strikes', () => {
+    const targetWord = 'abc';
+    const guessedWord = 'a_c'.split('');
+    const maxStrikes = 5;
+    hangman.playHangman(targetWord, guessedWord, maxStrikes, maxStrikes);
+
+    expect(console.log).toBeCalledTimes(1);
+    expect(console.log).toBeCalledWith(`You lose! The word was ${targetWord}`);
+    expect(hangmanSpy).toBeCalledTimes(1);
+  });
+
+  it('should continue play if there was no win or loss', () => {});
 });
